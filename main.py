39531f9a3156c1054b046faaeab7d049d839d146
steeve0403 <zychsteeve4@gmail.com>
2024-09-03 12:21:54 +0200

    transform_perspective()

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class MainWidget(Widget):
     point_perspective_x = NumericProperty(0)
     point_perspective_y = NumericProperty(0)
 
-    V_NB_LINES = 7  # Preserves symmetry
+    V_NB_LINES = 7  # Preserves symmetry => Use odd number
     V_LINES_SPACING = .1  # percentage in screen width
     vertical_lines = []
 
@@ -46,13 +46,31 @@ class MainWidget(Widget):
         spacing = self.V_LINES_SPACING * self.width
         offset = -int(self.V_NB_LINES / 2)
         for i in range(0, self.V_NB_LINES):
-            x1 = int(central_line_x + offset * spacing)
-            y1 = 0
-            x2 = x1
-            y2 = self.height
+            line_x = int(central_line_x + offset * spacing)
+            x1, y1 = self.transform(line_x, 0)
+            x2, y2 = self.transform(line_x, self.height)
 
             self.vertical_lines[i].points = [x1, y1, x2, y2]
             offset += 1
+
+    def transform(self, x, y):
+        # return self.transform_2D(x, y)
+        return self.transform_perspective(x, y)
+
+    def transform_2D(self, x, y):
+        return x, y
+
+    def transform_perspective(self, x, y):
+        tr_y = y * self.point_perspective_y / self.height
+        if tr_y > self.point_perspective_x:
+            tr_y = self.point_perspective_y
+
+        diff_x = x - self.point_perspective_x
+        diff_y = self.point_perspective_y - tr_y
+        offset_x = diff_x * diff_y / self.point_perspective_y
+
+        tr_x = self.point_perspective_x + offset_x
+        return tr_x, tr_y
 
 
 class GalaxyApp(App):
