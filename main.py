2713798806338c0432960adecc62ff76e9d5cbc0
steeve0403 <zychsteeve4@gmail.com>
2024-09-04 16:54:42 +0200

    refactor code and improvement structure files

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.uix import widget
 from kivy.uix.relativelayout import RelativeLayout
 
 from src.effects.audio_manager import AudioManager
+from src.effects.visual_manager import VisualManager
 
 Config.set('graphics', 'width', '900')
 Config.set('graphics', 'height', '550')
@@ -17,30 +18,18 @@ from kivy.properties import NumericProperty, Clock, ObjectProperty, StringProper
 from src.utils.constants import *
 from src.utils.transforms import transform
 
-Builder.load_file('menu.kv')
+Builder.load_file('src/kv_files/menu.kv')
 
 
 class MainWidget(RelativeLayout):
-    from src.utils.transforms import transform
     from src.utils.user_action import keyboard_closed, on_keyboard_up, on_keyboard_down
     menu_widget = ObjectProperty()
-    point_perspective_x = NumericProperty(0)
-    point_perspective_y = NumericProperty(0)
-
-    # Vertical Lines
-    vertical_lines = []
-
-    # Horizontal lines
-    horizontal_lines = []
 
     current_offset_y = 0
     current_y_loop = 0
 
     current_speed_x = 0
     current_offset_x = 0
-
-    tiles = []
-    tiles_coordinates = []
 
     ship = None
     ship_coordinates = [(0, 0), (0, 0), (0, 0)]
@@ -53,15 +42,16 @@ class MainWidget(RelativeLayout):
 
     score_txt = StringProperty()
 
-
-
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         # print("Init W:" + str(self.width) + "H:" + str(self.height))
         self.audio_manager = AudioManager()
-        self.init_vertical_lines()
-        self.init_horizontal_lines()
-        self.init_tiles()
+        self.visual_manager = VisualManager(self)
+
+        self.visual_manager.init_vertical_lines()
+        self.visual_manager.init_horizontal_lines()
+        self.visual_manager.init_tiles()
+
         self.init_ship()
         self.reset_game()
 
@@ -80,9 +70,9 @@ class MainWidget(RelativeLayout):
         self.current_offset_x = 0
         self.score_txt = "Score: " + str(self.current_y_loop)
 
-        self.tiles_coordinates = []
-        self.pre_fill_tiles_coordinates()
-        self.generate_tiles_coordinates()
+        self.visual_manager.tiles_coordinates = []
+        self.visual_manager.pre_fill_tiles_coordinates()
+        self.visual_manager.generate_tiles_coordinates()
         self.state_game_over = False
 
     def is_desktop(self):
@@ -114,8 +104,8 @@ class MainWidget(RelativeLayout):
         self.ship.points = [x1, y1, x2, y2, x3, y3]
 
     def check_ship_collision(self):
-        for i in range(0, len(self.tiles_coordinates)):
-            tile_x, tile_y = self.tiles_coordinates[i]
+        for i in range(0, len(self.visual_manager.tiles_coordinates)):
+            tile_x, tile_y = self.visual_manager.tiles_coordinates[i]
             if tile_y > self.current_y_loop + 1:
                 return False
             if self.check_ship_collision_with_tile(tile_x, tile_y):
@@ -123,137 +113,21 @@ class MainWidget(RelativeLayout):
         return False
 
     def check_ship_collision_with_tile(self, tile_x, tile_y):
-        x_min, y_min = self.get_tile_coordinates(tile_x, tile_y)
-        x_max, y_max = self.get_tile_coordinates(tile_x + 1, tile_y + 1)
+        x_min, y_min = self.visual_manager.get_tile_coordinates(tile_x, tile_y)
+        x_max, y_max = self.visual_manager.get_tile_coordinates(tile_x + 1, tile_y + 1)
         for i in range(0, 3):
             p_x, p_y = self.ship_coordinates[i]
             if x_min <= p_x <= x_max and y_min <= p_y <= y_max:
                 return True
         return False
 
-    def init_tiles(self):
-        with self.canvas:
-            Color(1, 1, 1)
-            for i in range(0, NB_TILES):
-                self.tiles.append(Quad())
-
-    def pre_fill_tiles_coordinates(self):
-        for i in range(0, 10):
-            self.tiles_coordinates.append((0, i))
-
-    def generate_tiles_coordinates(self):
-        last_y = 0
-        last_x = 0
-
-        for i in range(len(self.tiles_coordinates) - 1, -1, -1):
-            if self.tiles_coordinates[i][1] < self.current_y_loop:
-                del self.tiles_coordinates[i]
-
-        if len(self.tiles_coordinates) > 0:
-            last_coordinates = self.tiles_coordinates[-1]
-            last_x = last_coordinates[0]
-            last_y = last_coordinates[1] + 1
-
-        for i in range(len(self.tiles_coordinates), NB_TILES):
-            r = random.randint(0, 2)
-            # 0 -> all right
-            # 1 -> right
-            # 2 -> left
-            start_index = -int(V_NB_LINES / 2) + 1
-            end_index = start_index + V_NB_LINES - 1
-            if last_x <= start_index:
-                r = 1
-            if last_x >= end_index:
-                r = 2
-            self.tiles_coordinates.append((last_x, last_y))
-            if r == 1:
-                last_x += 1
-                self.tiles_coordinates.append((last_x, last_y))
-                last_y += 1
-                self.tiles_coordinates.append((last_x, last_y))
-            if r == 2:
-                last_x -= 1
-                self.tiles_coordinates.append((last_x, last_y))
-                last_y += 1
-                self.tiles_coordinates.append((last_x, last_y))
-            last_y += 1
-
-    def init_vertical_lines(self):
-        with self.canvas:
-            Color(1, 1, 1)
-            for i in range(0, V_NB_LINES):
-                self.vertical_lines.append(Line())
-
-    def get_line_x_from_index(self, index):
-        central_line_x = self.point_perspective_x
-        spacing = V_LINES_SPACING * self.width
-        offset = index - 0.5
-        line_x = central_line_x + offset * spacing + self.current_offset_x
-        return line_x
-
-    def get_line_y_from_index(self, index):
-        spacing_y = H_LINES_SPACING * self.height
-        line_y = index * spacing_y - self.current_offset_y
-        return line_y
-
-    def get_tile_coordinates(self, tile_x, tile_y):
-        tile_y = tile_y - self.current_y_loop
-        x = self.get_line_x_from_index(tile_x)
-        y = self.get_line_y_from_index(tile_y)
-        return x, y
-
-    def update_tiles(self):
-        for i in range(0, NB_TILES):
-            tile = self.tiles[i]
-            tile_coordinates = self.tiles_coordinates[i]
-
-            x_min, y_min = self.get_tile_coordinates(tile_coordinates[0], tile_coordinates[1])
-            x_max, y_max = self.get_tile_coordinates(tile_coordinates[0] + 1, tile_coordinates[1] + 1)
-
-            #   2   3
-            #
-            #   1   4
-            x1, y1 = transform(self, x_min, y_min)
-            x2, y2 = transform(self, x_min, y_max)
-            x3, y3 = transform(self, x_max, y_max)
-            x4, y4 = transform(self, x_max, y_min)
-
-            tile.points = [x1, y1, x2, y2, x3, y3, x4, y4]
-
-    def update_vertical_lines(self):
-        start_index = -int(V_NB_LINES / 2) + 1
-        for i in range(start_index, start_index + V_NB_LINES):
-            line_x = self.get_line_x_from_index(i)
-            x1, y1 = transform(self, line_x, 0)
-            x2, y2 = transform(self, line_x, self.height)
-            self.vertical_lines[i].points = [x1, y1, x2, y2]
-
-    def init_horizontal_lines(self):
-        with self.canvas:
-            Color(1, 1, 1)
-            for i in range(0, H_NB_LINES):
-                self.horizontal_lines.append(Line())
-
-    def update_horizontal_lines(self):
-        start_index = -int(V_NB_LINES / 2) + 1
-        end_index = start_index + V_NB_LINES - 1
-
-        x_min = self.get_line_x_from_index(start_index)
-        x_max = self.get_line_x_from_index(end_index)
-
-        for i in range(0, H_NB_LINES):
-            line_y = self.get_line_y_from_index(i)
-            x1, y1 = transform(self, x_min, line_y)
-            x2, y2 = transform(self, x_max, line_y)
-            self.horizontal_lines[i].points = [x1, y1, x2, y2]
-
     def update(self, dt):
         # print("dt : " + str(dt))
         time_factor = dt * 60
 
-        self.update_vertical_lines()
-        self.update_horizontal_lines()
-        self.update_tiles()
+        self.visual_manager.update_vertical_lines()
+        self.visual_manager.update_horizontal_lines()
+        self.visual_manager.update_tiles()
         self.update_ship()
 
         if not self.state_game_over and self.state_game_has_started:
@@ -265,7 +139,7 @@ class MainWidget(RelativeLayout):
                 self.current_offset_y -= spacing_y
                 self.current_y_loop += 1
                 self.score_txt = "Score: " + str(self.current_y_loop)
-                self.generate_tiles_coordinates()
+                self.visual_manager.generate_tiles_coordinates()
 
             speed_x = self.current_speed_x * self.width / 100
             self.current_offset_x += speed_x * time_factor
---
+++ src/effects/visual_manager.py
@@ -0,0 +1,145 @@
+# src/effects/visual_manager.py
+import random
+
+from kivy.graphics import Line, Color, Quad
+from kivy.properties import NumericProperty
+
+from src.utils.transforms import transform
+from src.utils.constants import *
+
+
+class VisualManager:
+    def __init__(self, widget):
+        self.widget = widget
+        self.point_perspective_x = NumericProperty(0)
+        self.point_perspective_y = NumericProperty(0)
+
+        self.vertical_lines = []
+
+        self.horizontal_lines = []
+
+        self.tiles = []
+        self.tiles_coordinates = []
+
+        # self.init_vertical_lines()
+        # self.init_horizontal_lines()
+        # self.init_tiles()
+
+    def init_tiles(self):
+        with self.widget.canvas:
+            Color(1, 1, 1)
+            for i in range(0, NB_TILES):
+                self.tiles.append(Quad())
+
+    def pre_fill_tiles_coordinates(self):
+        for i in range(0, 10):
+            self.tiles_coordinates.append((0, i))
+
+    def generate_tiles_coordinates(self):
+        last_y = 0
+        last_x = 0
+
+        for i in range(len(self.tiles_coordinates) - 1, -1, -1):
+            if self.tiles_coordinates[i][1] < self.widget.current_y_loop:
+                del self.tiles_coordinates[i]
+
+        if len(self.tiles_coordinates) > 0:
+            last_coordinates = self.tiles_coordinates[-1]
+            last_x = last_coordinates[0]
+            last_y = last_coordinates[1] + 1
+
+        for i in range(len(self.tiles_coordinates), NB_TILES):
+            r = random.randint(0, 2)
+            # 0 -> all right
+            # 1 -> right
+            # 2 -> left
+            start_index = -int(V_NB_LINES / 2) + 1
+            end_index = start_index + V_NB_LINES - 1
+            if last_x <= start_index:
+                r = 1
+            if last_x >= end_index:
+                r = 2
+            self.tiles_coordinates.append((last_x, last_y))
+            if r == 1:
+                last_x += 1
+                self.tiles_coordinates.append((last_x, last_y))
+                last_y += 1
+                self.tiles_coordinates.append((last_x, last_y))
+            if r == 2:
+                last_x -= 1
+                self.tiles_coordinates.append((last_x, last_y))
+                last_y += 1
+                self.tiles_coordinates.append((last_x, last_y))
+            last_y += 1
+
+    def init_vertical_lines(self):
+        """Initialiser les lignes verticales."""
+        with self.widget.canvas:
+            for _ in range(V_NB_LINES):
+                self.vertical_lines.append(Line())
+
+    def init_horizontal_lines(self):
+        """Initialiser les lignes horizontales."""
+        with self.widget.canvas:
+            for _ in range(H_NB_LINES):
+                self.horizontal_lines.append(Line())
+
+    def get_line_x_from_index(self, index):
+        """Calculer la position X d'une ligne basée sur son index."""
+        central_line_x = self.widget.point_perspective_x
+        spacing = V_LINES_SPACING * self.widget.width
+        offset = index - 0.5
+        line_x = central_line_x + offset * spacing + self.widget.current_offset_x
+        return line_x
+
+    def get_line_y_from_index(self, index):
+        spacing_y = H_LINES_SPACING * self.widget.height
+        line_y = index * spacing_y - self.widget.current_offset_y
+        return line_y
+
+    def get_tile_coordinates(self, tile_x, tile_y):
+        tile_y = tile_y - self.widget.current_y_loop
+        x = self.get_line_x_from_index(tile_x)
+        y = self.get_line_y_from_index(tile_y)
+        return x, y
+
+    def update_tiles(self):
+        for i in range(0, NB_TILES):
+            tile = self.tiles[i]
+            tile_coordinates = self.tiles_coordinates[i]
+
+            x_min, y_min = self.get_tile_coordinates(tile_coordinates[0], tile_coordinates[1])
+            x_max, y_max = self.get_tile_coordinates(tile_coordinates[0] + 1, tile_coordinates[1] + 1)
+
+            #   2   3
+            #
+            #   1   4
+            x1, y1 = transform(self, x_min, y_min)
+            x2, y2 = transform(self, x_min, y_max)
+            x3, y3 = transform(self, x_max, y_max)
+            x4, y4 = transform(self, x_max, y_min)
+
+            tile.points = [x1, y1, x2, y2, x3, y3, x4, y4]
+
+    def update_vertical_lines(self):
+        """Mettre à jour la position des lignes verticales."""
+        start_index = -int(V_NB_LINES / 2) + 1
+        for i in range(start_index, start_index + V_NB_LINES):
+            line_x = self.get_line_x_from_index(i)
+            x1, y1 = transform(self.widget, line_x, 0)
+            x2, y2 = transform(self.widget, line_x, self.widget.height)
+            self.vertical_lines[i].points = [x1, y1, x2, y2]
+
+    def update_horizontal_lines(self):
+        """Mettre à jour la position des lignes horizontales."""
+        start_index = -int(V_NB_LINES / 2) + 1
+        end_index = start_index + V_NB_LINES - 1
+
+        x_min = self.get_line_x_from_index(start_index)
+        x_max = self.get_line_x_from_index(end_index)
+
+        for i in range(0, H_NB_LINES):
+            line_y = self.get_line_y_from_index(i)
+            x1, y1 = transform(self, x_min, line_y)
+            x2, y2 = transform(self, x_max, line_y)
+            self.horizontal_lines[i].points = [x1, y1, x2, y2]
