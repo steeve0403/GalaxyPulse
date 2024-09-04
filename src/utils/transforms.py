72e69e3e21946f18708fd3d536d092ecb01f1157
steeve0403 <zychsteeve4@gmail.com>
2024-09-04 18:36:17 +0200

    Improvment code

--- src/utils/transforms.py
+++ src/utils/transforms.py
@@ -1,24 +1,19 @@
-def transform(self, x, y):
+def transform(widget, x, y):
     # return self.transform_2D(x, y)
-    return self.transform_perspective(x, y)
+    return transform_perspective(widget, x, y)
 
-
-def transform_2D(self, x, y):
+def transform_2D(widget, x, y):
     return int(x), int(y)
-
 
 def transform_perspective(self, x, y):
     linear_y = y * self.point_perspective_y / self.height
-    if linear_y > self.point_perspective_y:
-        linear_y = self.point_perspective_y
+    linear_y = min(linear_y, self.point_perspective_y)  # Limiter la valeur maximale
 
     diff_x = x - self.point_perspective_x
     diff_y = self.point_perspective_y - linear_y
-    factor_y = diff_y / self.point_perspective_y
-    factor_y = pow(factor_y, 4)
+    factor_y = (diff_y / self.point_perspective_y) ** 4  # Éviter plusieurs appels à pow()
 
     offset_x = diff_x * factor_y
-
     tr_x = self.point_perspective_x + offset_x
     tr_y = self.point_perspective_y - factor_y * self.point_perspective_y
     return int(tr_x), int(tr_y)
